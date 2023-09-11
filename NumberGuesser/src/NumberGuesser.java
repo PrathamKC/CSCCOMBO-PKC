adf3078319b9ad6c7a1a827e78952722858ec163
Kc <S02874502@ccc.ccofc.edu>
2023-09-11 15:53:38 -0600

    NumberGuesser

--- NumberGuesser/src/NumberGuesser.java
+++ NumberGuesser/src/NumberGuesser.java
@@ -1,23 +1,26 @@
 import java.util.Scanner;
+import java.util.Random;
 public class NumberGuesser 
 {
 	public static void main(String[] args) 
 	{
 		Scanner input = new Scanner(System.in);
-		int realNum = 0, userGuess;
+		Random realNum = new Random();
+		int random = realNum.nextInt(10), userGuess;
+		
 		
 		System.out.println("Guess a random number: ");
 		userGuess = input.nextInt();
 		
-		while (userGuess != realNum)
-			if (userGuess > realNum) 
+		while (userGuess != random)
+			if (userGuess > random) 
 				{
 					System.out.println("Too high!");
 					userGuess = input.nextInt();
 				}
 			else 
 				{
-					if (userGuess < realNum) 
+					if (userGuess < random) 
 						{
 							System.out.println("Too low!");
 							userGuess = input.nextInt();
