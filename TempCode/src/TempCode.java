a12dc5e9eb2c02577632a08b4c3b3d15283b4a40
Kc <S02874502@ccc.ccofc.edu>
2023-09-11 15:21:46 -0600

    TempCode

---
+++ TempCode/src/TempCode.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class TempCode 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		double fahrenheit = 0, kelvin = 0, celsius = 0;
+		
+		System.out.println("What is the temp in Fahreneit? ");
+		fahrenheit = input.nextDouble();
+		
+		celsius = (fahrenheit - 32) * (0.55555555);
+		kelvin = celsius + 273.15;
+		System.out.println("Celsius: " + celsius + "\n" + "Kelvin : " + kelvin);
+
+	}
+
+}
