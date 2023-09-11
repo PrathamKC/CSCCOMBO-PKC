0dbe2b685149eaa731aae74c6476fe6df48622db
Kc <S02874502@ccc.ccofc.edu>
2023-09-11 15:40:07 -0600

    NumberGuesser

--- NumberGuesser/src/NumberGuesser.java
+++ NumberGuesser/src/NumberGuesser.java
@@ -1,9 +1,28 @@
-
-public class NumberGuesser {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+import java.util.Scanner;
+public class NumberGuesser 
+{
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		int realNum = 0, userGuess;
+		
+		System.out.println("Guess a random number: ");
+		userGuess = input.nextInt();
+		
+		while (userGuess != realNum)
+			if (userGuess > realNum) 
+				{
+					System.out.println("Too high!");
+					userGuess = input.nextInt();
+				}
+			else 
+				{
+					if (userGuess < realNum) 
+						{
+							System.out.println("Too low!");
+							userGuess = input.nextInt();
+						}
+				}
+		System.out.println("Correct!");
 	}
-
 }
