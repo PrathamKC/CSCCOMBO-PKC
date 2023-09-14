4812345081b97723c4449d7691c36e1bbcab2566
Kc <S02874502@ccc.ccofc.edu>
2023-09-14 16:10:55 -0600

    ForLoop

---
+++ ForLoop/src/ForLoop.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class ForLoop
+{
+	public static void main(String[] args) 
+	{
+		//Scanner input = new Scanner(System.in);
+		//Scanner scnr = new Scanner(System.in);
+		int i = 1, sum = 0;
+		int j;
+		
+		
+		while (i <= 10) 
+			{
+				//i++;
+				sum = sum + i;
+				System.out.println(i + " Your sum is: " + sum);
+				i++;
+			}
+		
+		System.out.print("\n");
+		sum = 0;
+
+		for (i=1; i<=10; i++) 
+			{
+			
+			sum = sum + i;
+			System.out.println(i + " Your sum is: " + sum);
+			}
+		
+		for (j = 0; j<=5; j++) 
+		{
+			System.out.println("1 1 1 1 1");
+		}
+			
+	}
+}
