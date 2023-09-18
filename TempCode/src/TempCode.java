fa6db8528857868b068b478bfacbb2b938fc8037
Kc <S02874502@ccc.ccofc.edu>
2023-09-18 16:05:34 -0600

    TempCode

--- TempCode/src/TempCode.java
+++ TempCode/src/TempCode.java
@@ -4,13 +4,36 @@ public class TempCode
 	public static void main(String[] args) 
 	{
 		Scanner input = new Scanner(System.in);
-		double fahrenheit = 0, kelvin = 0, celsius = 0;
+		double fahrenheit, kelvin = 0, celsius = 0;
 		
-		System.out.println("What is the temp in Fahreneit? ");
-		fahrenheit = input.nextDouble();
+		//System.out.println("What is the temp in Fahreneit? ");
+		fahrenheit = getTemp();
 		
-		celsius = (fahrenheit - 32) * (0.55555555);
+		celsius = (fahrenheit - 32) * (5/9.0);
+		outputTemp("Celsius: ", celsius);
+		
 		kelvin = celsius + 273.15;
-		System.out.println("Celsius: " + celsius + "\n" + "Kelvin : " + kelvin);
+		outputTemp("Kelvin: ", kelvin);
+
+		//System.out.println("Celsius: " + celsius + "\n" + "Kelvin : " + kelvin);
+		
+		//outputTemp("Celsius: ", celsius, kelvin);
 	}
+	
+	public static void outputTemp(String scale, double temp) 
+	{
+		System.out.println(scale + temp);	
+	}
+	
+	public static double getTemp() //you can only return one variable with a double
+	{
+		Scanner input = new Scanner(System.in);
+		//Scanner keyboard;
+		double temp;
+		//keyboard = new Scanner(System.in); if you remove these two "//", then u have to delete the scanner thing in this ps double.
+		System.out.println("What is the temp in Fahreneit? ");
+		temp = input.nextDouble();
+		return temp;
+	}
+	
 }
