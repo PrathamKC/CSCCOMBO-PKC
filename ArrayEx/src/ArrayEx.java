7c6049f0b96942c1d8929c4d29f9ef2f80b88faa
Kc <S02874502@ccc.ccofc.edu>
2023-10-05 16:14:38 -0600

    ArrayEx

--- ArrayEx/src/ArrayEx.java
+++ ArrayEx/src/ArrayEx.java
@@ -1,26 +1,47 @@
 import java.lang.reflect.Array;
-
+import java.util.Arrays;
 public class ArrayEx 
 {
 	public static void main(String[] args) 
 	{
 		int[] arrOne = {1,2,3,4,5};
+		int[] arrTwo = {1,7,8,3,4};
+		int[] arrThree = new int [arrOne.length];
 		
-		if (findValue(arrOne, 6))
+		//#1
+		if (findValue(arrOne, 2))
 		{
-			System.out.println("Yes");
+			System.out.println("Match!");
 		}
 		else 
 		{
-			System.out.println("No");
+			System.out.println("No match!");
 		}
 		
+		
+		//#2
 		arrayReverse(arrOne);
-		for (int i =0; i < arrOne.length; i++)
+		for (int i = 0; i < arrOne.length; i++)
 		{
 			System.out.print(arrOne[i]+", ");
 		}
 		
+
+		//#3
+		arrThree = findCommon(arrOne, arrTwo);
+		System.out.println("\n"+ Arrays.toString(arrThree));
+		
+		
+		//#4
+		int evens = evenOdd(arrOne);
+		avg(arrOne);
+		System.out.println("Number of evens: " + evens);
+		System.out.println("Number of odds: " + (arrOne.length-evens));
+		
+		
+		//#5
+		int sum = avg(arrOne);
+		System.out.println("Your average is: " + sum/(arrOne.length-2));	
 	}
 	
 	public static boolean findValue(int[] arrOne, int a)
@@ -31,7 +52,6 @@ public class ArrayEx
 			{
 				return true;
 			}
-			
 		}
 		return false;
 	}
@@ -39,7 +59,6 @@ public class ArrayEx
 	public static int[] arrayReverse(int []arrOne)
 	{
 		int tempVal;
-		
 		for (int i = 0; i<arrOne.length/2;i++)
 		{
 			tempVal = arrOne[i];
@@ -49,8 +68,52 @@ public class ArrayEx
 		return arrOne;
 	}
 	
-//	public static int[] findCommon(int[] arrOne, int[] arrTwo)
-//	{
-//		
-//	}
+	public static int[] findCommon(int[] arrOne, int[] arrTwo)
+	{
+		int count = 0;
+		int [] arr = new int[arrOne.length];
+		for (int i = 0; i < arrOne.length; i++) 
+		{
+			for (int j = 0; j <arrTwo.length; j++)
+			{
+				if (arrOne[i] == arrTwo[j])
+				{
+					arr[count] = arrOne[i];
+					count++;
+				}
+			}
+		}	
+		return arr;
+	}
+	
+	public static int evenOdd(int[] arrOne)
+	{
+		int even = 0;
+		for(int i = 0; i < arrOne.length; i++)
+		{
+			if (arrOne[i] %2 == 0)
+			{
+				even++;
+			}
+		}
+		return even;
+	}
+	
+	public static int avg(int[] arrOne)
+	{
+		int max = arrOne[0], min = arrOne[0], sum = 0;
+		for (int i = 0; i < arrOne.length; i++) 
+		{
+			if (arrOne[i] < min)
+			{
+				min = arrOne[i];
+			}
+			if (arrOne[i] > max)
+			{
+				max = arrOne[i];
+			}
+			sum += arrOne[i];
+		}
+		return sum-min-max;
+	}
 }
